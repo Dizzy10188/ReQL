d9bb1c0a4db88ce1b16dd506f42d843cb4b0e327
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2020-04-22 16:39:18 -0600

    created new methdods

--- ReQLAssignment/src/main/java/ReQLPackage/ReQL.java
+++ ReQLAssignment/src/main/java/ReQLPackage/ReQL.java
@@ -6,9 +6,37 @@ import java.io.InputStreamReader;
 
 public class ReQL {
     public static void main(String[] args) {
+        createTable();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String line;
+//        try {
+//            line = br.readLine();
+//            while(line != null) {
+//                if (line.contains(";")) {
+//                    break;
+//                }
+//                else {
+//                    line += "\n" + br.readLine();
+//                }
+//            }
+//            System.out.println("you typed: " + line);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public static void createTable() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
         try {
+            System.out.println("Please define the table structure");
+            System.out.println("EX: CREATE TABLE *TABLE_NAME* (\n" +
+                    "COLUMN_NAME_2,\n" +
+                    "COLUMN_NAME_1,\n" +
+                    "etc...\n" +
+                    ")\n" +
+                    ": line format ((regex 1);(regex 2);etc...\n" +
+                    "file \"path to file with extension\"\n");
             line = br.readLine();
             while(line != null) {
                 if (line.contains(";")) {
@@ -18,10 +46,14 @@ public class ReQL {
                     line += "\n" + br.readLine();
                 }
             }
-            System.out.println("you typed: " + line);
+            System.out.println("---------------------------");
+            System.out.println(line);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void selectFromTable() {
 
     }
 }
