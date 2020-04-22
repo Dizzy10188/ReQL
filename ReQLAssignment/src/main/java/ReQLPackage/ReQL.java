27f653f2804977bd4ef773026897a9acd4c12464
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2020-04-22 11:49:13 -0600

    Test Code

--- ReQLAssignment/src/main/java/ReQLPackage/ReQL.java
+++ ReQLAssignment/src/main/java/ReQLPackage/ReQL.java
@@ -1,4 +1,27 @@
 package ReQLPackage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ReQL {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        try {
+            line = br.readLine();
+            while(line != null) {
+                if (line.contains(";")) {
+                    break;
+                }
+                else {
+                    line += "\n" + br.readLine();
+                }
+            }
+            System.out.println("you typed: " + line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
